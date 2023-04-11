c0087898e4f50aace261396d976d6ed7dd3fdbda
alex123 <alex123@dot.com>
2023-04-11 19:50:55 +0500

    Task_02 Add name column XYZF

--- task_02.py
+++ task_02.py
@@ -2,6 +2,7 @@ def task_condition():
     print("\nЗадача 2. Выведите таблицу истинности для выражения ¬(X ∧ Y) ∨ Z. \n")
 
 def print_result():
+    print("X\tY\tZ\tF\n")
     for x in range(0, 2):
         for y in range(0, 2):
             for z in range(0, 2):
